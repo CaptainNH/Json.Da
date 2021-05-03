8447a10b994734d66b42129dcc53b338aa3279eb
RubikRr <vova.chakalov.2003@gmail.com>
2021-05-03 19:41:04 +0300

    Закоментил класс WorkLoad

--- Json.Da/Workload.cs
+++ Json.Da/Workload.cs
@@ -8,5 +8,12 @@ namespace Json.Da
 {
     class Workload
     {
+        //public string Employee { get; set;}
+        //public string Discipline { get; set; }
+        //public int Year { get; set;}
+        //public double Hours { get; set; }
+        //public double Lecture { get; set; }
+        //public double Practice { get; set; }
+        //public double Labs { get; set; }
     }
 }
