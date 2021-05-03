81b9d895e92494d00cce2488dea86e50fc21cff3
Стас <stas08072002@gmail.com>
2021-05-04 00:43:52 +0300

    Перешли на ClosedXML

--- Json.Da/AddEmployee.cs
+++ Json.Da/AddEmployee.cs
@@ -4,25 +4,33 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using ClosedXML.Excel;
 
 namespace Json.Da
 {
     class AddEmployee
     {
+        static IXLWorksheet OpenExcelFile(string filePath, string sheetName)
+        {
+            var xlBook = new XLWorkbook(filePath);
+            var xlSheet = xlBook.Worksheet(sheetName);
+            return xlSheet;
+        }
+
         static List<Employee> GenerateList()
         {
             string path = Environment.CurrentDirectory;
             var empList = new List<Employee>();
-            var xlApp = new Microsoft.Office.Interop.Excel.Application();
-            var xlBook = xlApp.Workbooks.Open(path + @"\..\..\Documents\Svedenia.xlsx");
-            var xlSheet = xlBook.Worksheets["Сведения о преподавателях"];
+            var xlSheetSvedenia = OpenExcelFile(path + @"\..\..\Documents\Svedenia.xlsx", "Сведения о преподавателях");
+            var xlSheetNagruzki = OpenExcelFile(path + @"\..\..\Documents\Nagruzki.xlsx", "Сводное поручение");
+            var range = xlSheetNagruzki.Range("B13:B19");
             for (int i = 3; i <= 120; i++)
             {
-                string[] fio = xlSheet.Cells[1][i].Value.Split();
+                string[] fio = xlSheetSvedenia.Cell($"A{i}").GetValue<string>().Split();
                 string surname = fio[0].Trim();
                 string name = fio[1].Trim();
                 string fathername = fio[2].Trim();
-                string[] s = xlSheet.Cells[3][i].Value.Split(',');
+                string[] s = xlSheetSvedenia.Cell($"C{i}").GetValue<string>().Split(',');
                 string pos = s[0].Trim().Split(new string[] { "Должность" }, StringSplitOptions.None)[1].Trim(new char[] { ' ', '-', '–' });
                 string rank = "-";
                 if (s.Length == 3)
@@ -30,8 +38,7 @@ namespace Json.Da
                     if (!s[2].ToLower().Contains("отсутствует"))
                         rank = s[2].Trim();
                 }
-                else
-                    if (!s[3].ToLower().Contains("отсутствует"))
+                else if (!s[3].ToLower().Contains("отсутствует"))
                     rank = s[3].Trim();
 
                 var prepod = new Employee()
@@ -45,13 +52,6 @@ namespace Json.Da
 
                 empList.Add(prepod);
             }
-            if (xlApp != null)
-            {
-                xlApp.Quit();
-                Marshal.ReleaseComObject(xlSheet);
-                Marshal.ReleaseComObject(xlApp);
-            }
-
             return empList;
         }
 
